18913200de41b78d6252ae840f2a329d856e98b9
Harshavardhan <harshavardhanr.993@gmail.com>
2021-01-16 21:53:24 +0530

    Used a stack based approach to propagate schema from XML to JSON
    Issues:
    1. Structure of JSON is not **exactly** same as the given examples
    2. Edge case related to Root tag still needs to be handled

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from xml.etree import ElementTree as ET
 import logging
 
+
 def upload_page(request):
     if request.method == 'POST':
         try:
@@ -14,7 +15,9 @@ def upload_page(request):
             return JsonResponse({"Error": pe.msg})
     return render(request, "upload_page.html")
 
+
 logger = logging.getLogger(__name__)
+
 
 def save_uploaded_file(file):
     filename = file.name.replace(".xml", f"-{int(datetime.now().timestamp())}.xml")
@@ -26,14 +29,38 @@ def save_uploaded_file(file):
 
 
 def pullparse(file):
-    parser = ET.XMLPullParser(["start", "end"])
-    result = []
+    pull_parser = ET.XMLPullParser(["start", "end"])
+    stream_parser = StreamParser()
+
     for chunk in file.chunks():
         # Look for events in the read chunk
-        parser.feed(chunk)
+        pull_parser.feed(chunk)
         # Iterate through events found
-        for event, element in parser.read_events():
-            if event == "start" and element.text:
-                logger.warning(f"<{element.tag}> : {element.text.strip()}")
-                result.append((element.tag, element.text))
-    return dict(result)
+        for event, element in pull_parser.read_events():
+            stream_parser.build(event, element)
+    result = stream_parser.stack
+    return result
+
+
+class StreamParser():
+    def __init__(self):
+        self.stack = []
+
+    def build(self, event, element):
+        if event == "start":
+            self.stack.append({"tag": element.tag, "value": element.text.strip()})
+        else:
+            top_item = self.stack.pop()
+            if top_item["tag"] == element.tag:
+                try:
+                    previous_item = self.stack.pop()
+                    if type(previous_item["value"]) is list:
+                        previous_item["value"].append(top_item)
+                    else:
+                        previous_item["value"] = [top_item]
+                    self.stack.append(previous_item)
+                except IndexError:
+                    logger.warning("Reached EOF of the XML file. File parsed")
+                    self.stack = top_item
+            else:
+                logger.warning("Malformed XML")
