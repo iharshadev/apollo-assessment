5aa7f87c92bc3861d801f648febf28cd02e1952a
Harshavardhan <harshavardhanr.993@gmail.com>
2021-01-16 19:25:26 +0530

    Updated `README.md` Pull Parser API approach
    1. Set up the XMLPullParser API to identify events for opening and closing of tags.
    2. Logic to form the JSON without the XML schema is complex. Still evaluating approaches
    
    Issues:
    Unable to form the JSON that has the same schema as the XML file while parsing it in chunks/streams

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -1,9 +1,39 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-
+from datetime import datetime
+from xml.etree import ElementTree as ET
+import logging
 
 def upload_page(request):
     if request.method == 'POST':
-        file = request.FILES["file"].read().decode("utf-8")
-        return JsonResponse({"content": file})
+        try:
+            # saved_filename = save_uploaded_file(request.FILES["file"])
+            result = pullparse(request.FILES["file"])
+            return JsonResponse(result)
+        except ET.ParseError as pe:
+            return JsonResponse({"Error": pe.msg})
     return render(request, "upload_page.html")
+
+logger = logging.getLogger(__name__)
+
+def save_uploaded_file(file):
+    filename = file.name.replace(".xml", f"-{int(datetime.now().timestamp())}.xml")
+    filename = f"static/uploaded-files/{filename}"
+    with open(filename, "wb+") as target:
+        for chunk in file.chunks():
+            target.write(chunk)
+    return filename
+
+
+def pullparse(file):
+    parser = ET.XMLPullParser(["start", "end"])
+    result = []
+    for chunk in file.chunks():
+        # Look for events in the read chunk
+        parser.feed(chunk)
+        # Iterate through events found
+        for event, element in parser.read_events():
+            if event == "start" and element.text:
+                logger.warning(f"<{element.tag}> : {element.text.strip()}")
+                result.append((element.tag, element.text))
+    return dict(result)
